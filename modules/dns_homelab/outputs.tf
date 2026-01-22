5b81da9aded680707bf7a3ce229b2deeef04c108
Brian Anderson <brian@briananderson.xyz>
2026-01-22 07:31:24 -0700

    Fix module outputs for v4 provider compatibility

--- modules/dns_homelab/outputs.tf
+++ modules/dns_homelab/outputs.tf
@@ -1,4 +1,4 @@
 output "record_names" {
   description = "List of all homelab DNS record names"
-  value       = [for record in cloudflare_dns_record.homelab : record.name]
+  value       = [for record in cloudflare_record.homelab : record.name]
 }
