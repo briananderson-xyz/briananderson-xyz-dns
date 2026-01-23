9d1a8b6d4b747cbdf29b083386c8a87c1eb76d20
Brian Anderson <brian@briananderson.xyz>
2026-01-23 01:04:53 -0700

    Fix: add cloudflare_api_token variable (required by provider)

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -1,4 +1,10 @@
+variable "cloudflare_api_token" {
+  type        = string
+  description = "Cloudflare API Token with DNS:Edit and Zone:Read scopes"
+  sensitive   = true
+}
+
 provider "cloudflare" {
-  # Cloudflare API token is read from CLOUDFLARE_API_TOKEN environment variable
+  api_token = var.cloudflare_api_token
 }
 
