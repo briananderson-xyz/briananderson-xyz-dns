c9ec29ac8946689dfa8a761cda8e3fcd2bb6e4d8
Brian Anderson <brian@briananderson.xyz>
2026-01-24 09:12:49 -0700

    fix: remove explicit api_token config to use CLOUDFLARE_API_TOKEN env var

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -1,10 +1,2 @@
-variable "cloudflare_api_token" {
-  type        = string
-  description = "Cloudflare API Token with DNS:Edit and Zone:Read scopes"
-  sensitive   = true
-}
-
-provider "cloudflare" {
-  api_token = var.cloudflare_api_token
-}
+provider "cloudflare" {}
 
