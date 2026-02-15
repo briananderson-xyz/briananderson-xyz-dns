05537e0633afd52ed2fa546ca44839c65197ef48
Brian Anderson <brian.anderson1222@gmail.com>
2026-02-14 21:47:19 -0700

    ci: upgrade to TF 1.11 and enable GCS native state locking

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket = "briananderson-xyz-tf-state"
-    prefix = "dns/prod"
+    bucket       = "briananderson-xyz-tf-state"
+    prefix       = "dns/prod"
+    use_lockfile = true
   }
 }
